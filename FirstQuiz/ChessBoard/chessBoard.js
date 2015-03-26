d936a7edeff3f1989a140f740cfff2b759a1d7f5
sleepbaer <sleepbear669@gmail.com>
2015-03-27 01:21:04 +0900

    solve quiz chessBoard

--- FirstQuiz/ChessBoard/chessBoard.js
+++ FirstQuiz/ChessBoard/chessBoard.js
@@ -1,6 +1,30 @@
-    var board = document.getElementById("board");
+    var mousedown = function(e) {
+    	if(e.target.id !== "board"){
+    	boardColor = e.target.className;
+    	e.target.className = "red";
+   		}
+    };
+    var mouseup = function(e) {
+    	if(e.target.id !== "board"){
+    	e.target.className = boardColor;
+    	}
+    };
+    
+ 	var str ="";
+ 	var template = getInnerHTMLById("boardTemplate");
+ 	var color;
     for(var i=0; i<4; i++){
         for(var j=0; j<4; j++){
-            // TODO : 여기에 필요한 코드를 작성해주세요.
+            if((i+j)%2 === 0) color = "white";
+            else color = "black";
+            str += template.replace("{color}",color);
         }
+        str+="<br>";
     }
+    innerHTMLById("board",str);
+
+    var board = getById("board");
+    var boardColor ='';
+
+    board.addEventListener("mousedown", mousedown);
+    board.addEventListener("mouseup", mouseup);
