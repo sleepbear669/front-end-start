fa9164d51481cb6e4a7bea3804a01720ee8d6c1a
sleepbaer <sleepbear669@gmail.com>
2015-03-27 23:56:43 +0900

    add checkList function

--- 02/ToDoList/todos.js
+++ 02/ToDoList/todos.js
@@ -1,17 +1,33 @@
 var toDoList = getById("toDoList");
 var template = getInnerHTMLById("toDoListTemplate");
+var listCount = localStorage.length;
+var contenList;
 
+var checkList = function(e) {
+	if(e.target.className === "toggle-checked"){
+		if(e.target.parentNode.className === "blue"){
+			e.target.parentNode.className = "";
+		}else{
+			e.target.parentNode.className = "blue";
+		}
+		
+	}
+};
 var deleteList =  function(e) {
-	if(e.target.className === "delete" || e.target.className === "toggle-checked"){
+	if(e.target.className === "delete"){
 			e.target.parentNode.remove();
 	}
 };
+
+
 var addTodoList = function(e){
-	if(e.which === 13){
+	if(e.which === 13 && e.target.value !== ""){
 		toDoList.innerHTML += template.replace("{content}",e.target.value);
 		e.target.value = null;
 	}
-}
+};
+
 
 getById("toDoText").addEventListener("keydown", addTodoList);
 toDoList.addEventListener("click", deleteList);
+toDoList.addEventListener("click", checkList);
