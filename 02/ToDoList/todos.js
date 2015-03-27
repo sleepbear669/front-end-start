0c7a584a79a685a634375e97626fa3966a403063
sleepbaer <sleepbear669@gmail.com>
2015-03-27 22:46:13 +0900

    add function addTodoList

--- 02/ToDoList/todos.js
+++ 02/ToDoList/todos.js
@@ -1,8 +1,12 @@
-//start...
+var toDoList = getById("toDoList");
+var template = getInnerHTMLById("toDoListTemplate");
 var input = getById("toDoText");
-input.addEventListener("keydown", function(e){
-	console.log(e.which	);
+
+var addTodoList = function(e){
 	if(e.which === 13){
+		toDoList.innerHTML += template.replace("{content}",e.target.value);
 		e.target.value = null;
 	}
-});
+}
+
+input.addEventListener("keydown", addTodoList);
