5fbea07c47317c83223ea56f610dd4b6feb955ef
sleepbaer <sleepbear669@gmail.com>
2015-03-28 12:33:55 +0900

    add tmpl.js  and use tmpl

--- 02/ToDoList/todos.js
+++ 02/ToDoList/todos.js
@@ -2,32 +2,45 @@ var toDoList = getById("toDoList");
 var template = getInnerHTMLById("toDoListTemplate");
 var listCount = localStorage.length;
 var contenList;
-
+var spanText;
 var checkList = function(e) {
 	if(e.target.className === "toggle-checked"){
 		if(e.target.parentNode.className === "blue"){
 			e.target.parentNode.className = "";
+			spanText = getNChild(e.target.parentNode, 3).textContent;
+			localStorage[spanText] = "";
 		}else{
 			e.target.parentNode.className = "blue";
+			spanText = getNChild(e.target.parentNode, 3).textContent;
+			localStorage[spanText] = "blue";
+
 		}
-		
 	}
 };
 var deleteList =  function(e) {
 	if(e.target.className === "delete"){
 			e.target.parentNode.remove();
 	}
+	spanText = getNChild(e.target.parentNode, 3).textContent;
+	localStorage.removeItem(spanText);
 };
 
 
 var addTodoList = function(e){
+	var content={};
 	if(e.which === 13 && e.target.value !== ""){
-		toDoList.innerHTML += template.replace("{content}",e.target.value);
+		content[e.target.value] = "";
+		toDoList.innerHTML += tmpl(template, {list:content});
+		localStorage.setItem(e.target.value, "");
 		e.target.value = null;
 	}
 };
+var loadData = function() {
+	var listHTML = tmpl(template, {list : localStorage});
+	innerHTMLById("toDoList", listHTML);
+}
 
-
+loadData();
 getById("toDoText").addEventListener("keydown", addTodoList);
 toDoList.addEventListener("click", deleteList);
 toDoList.addEventListener("click", checkList);
--- baseFunctionCode.js
+++ baseFunctionCode.js
@@ -1,3 +1,4 @@
     function getById(id){ return document.getElementById(id); }
     function getInnerHTMLById (id) { return getById(id).innerHTML; }
-    function innerHTMLById(id, str){ getById(id).innerHTML = str;  }
+    function innerHTMLById(id, str){ getById(id).innerHTML = str;  }   
+	function getNChild(element, n){return element.children[n-1];}
