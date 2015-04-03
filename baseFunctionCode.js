67a91b26ac52e3fe872fce5a56f0fab383368c48
sleepbear669 <sleepbear669@gmail.com>
2015-04-03 20:11:10 +0900

    03 report commit use DaumOpenAPi research

--- baseFunctionCode.js
+++ baseFunctionCode.js
@@ -1,4 +1,4 @@
-    var Time = {
+﻿    var Time = {
     "lastClickTime" : new Date().getTime(),
     currentTime : function(){ return new Date().getTime();},
     checkTime : function(){ this.lastClickTime = currentTime();},
@@ -13,3 +13,36 @@
     function innerHTMLById(id, str){ getById(id).innerHTML = str;  }   
 	function getNChild(element, n){return element.children[n-1];}
 	function currentTime(){ return new Date().getTime(); }
+
+    var htmlSpecialChars = function(string, reverse) {
+
+    var specialChars = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            "\"": "&quot;"
+        },
+        x;
+
+    if (typeof(reverse) != "undefined") {
+        reverse = [];
+        for (x in specialChars)
+            reverse.push(x);
+
+        reverse.reverse();
+
+        for (x = 0; x < reverse.length; x++)
+            string = string.replace(
+            new RegExp(specialChars[reverse[x]], "g"),
+            reverse[x]
+        );
+        return string;
+    }
+
+
+    for (x in specialChars)
+        string = string.replace(new RegExp(x, "g"), specialChars[x]);
+
+
+    return string;
+};
