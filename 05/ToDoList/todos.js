7946718bce895ed0c5070bcddd462277454cc5c4
sleepbear669 <sleepbear669@gmail.com>
2015-04-28 17:16:56 +0900

    05/TodoList project version 1.0

--- 05/ToDoList/todos.js
+++ 05/ToDoList/todos.js
@@ -1,7 +1,8 @@
 //start...
 $(document).ready(function(){
+    var $ul = $("ul");
+    $ul.html(localStorage["content"]);
     var todoContent = "";
-
     var inputTodo = function (selector) {
         todoContent = $(selector).val();
         $(selector).val("");
@@ -10,9 +11,26 @@ $(document).ready(function(){
     var addTodoList = function(content) {
         $.tmpl($("#todoTemplate"), {content : content}).appendTo("ul");
     };
+
+    function deleteContent(element) {
+        element.parentNode.remove();
+    }
+
+    $ul.click(function(event){
+        if(event.target.className === "delete"){
+            deleteContent(event.target);
+            updateLocalStorage("content", $ul.html());
+        }
+    });
     $("form").submit(function(){
         inputTodo("#todo_input");
-        addTodoList(todoContent);
+        if(todoContent !== "") {
+            addTodoList(todoContent);
+            updateLocalStorage("content", $ul.html());
+        }
     });
-    console.log("Hello Wolrd");
+    function updateLocalStorage(key , value){
+        localStorage[key] = value;
+    }
+    console.log("Hello World");
 });
