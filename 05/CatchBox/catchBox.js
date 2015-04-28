bf875d356d10d23f96887abcbb8169bbc827273d
sleepbear669 <sleepbear669@gmail.com>
2015-04-28 11:33:21 +0900

    05/CatchBox project version 1.0

--- 05/CatchBox/catchBox.js
+++ 05/CatchBox/catchBox.js
@@ -1,3 +1,13 @@
 /**
  * Created by sleepbear on 2015-04-23.
  */
+$( document ).ready(function() {
+    $(".container").height($(window).innerHeight());
+    var $box = $("<div>")
+        .attr("id", "game_box")
+        .height(30)
+        .width(30)
+        .appendTo("#game_screen");
+    console.log($box);
+});
+
